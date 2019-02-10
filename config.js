9ff68e4411cc0ca9f995a5992f6559a2419b30c0
CryingNexus <47480769+CryingNexus@users.noreply.github.com>
2019-02-10 15:53:18 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+    "TOKEN": "NTQ0MTc0ODM0Mjg0NzU3MDEy.D0HSGw.jfXyMwMQTglWQrLkdr-bpOC-VBM",
+    "MODMAIL_API_TOKEN": "MyApiToken",
+    "GUILD_ID": 401883208511389716,
+    "OWNERS": "216667001584746498,349834839966482432,142074241825636352",
+    "MONGO_URI": "mongodb+srv://CryingNexus:Neekilasn2015@cluster0-r9e42.mongodb.net/",
+    "logChannelId": "542772879880880128",
+    "newThreadCategoryId":"542772805003902976",
+
+  "status": "Message me for help!",
+  "responseMessage": "Thank you for your message! Our mod team will reply to you here as soon as possible."
+}
